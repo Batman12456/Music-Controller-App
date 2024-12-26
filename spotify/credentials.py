8e71130f5fa3eca73e227a47eef822aec4660d25
Rohan Sharma <sharmarrohan11@gmail.com>
2024-12-26 19:44:51 +0530

    Initial commit

---
+++ spotify/credentials.py
@@ -0,0 +1,11 @@
+REDIRECT_URI = "http://localhost:8000/spotify/redirect"
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+print(f"Client ID: {CLIENT_ID}, Client Secret: {CLIENT_SECRET}")
